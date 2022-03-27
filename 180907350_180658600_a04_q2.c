014607f487b8f6cb2f49cb94113e15270b9a45d0
Maroyan Putros <31706795+maroyanp@users.noreply.github.com>
2022-03-27 00:18:40 -0400

    Create 180907350_180658600_a04_q2.c
    just initializing the src file for later

---
+++ 180907350_180658600_a04_q2.c
@@ -0,0 +1,18 @@
+/*
+-----------------------------------------------------
+GITHUB NAMES:
+    maroyanp
+    Jake-77
+-----------------------------------------------------
+GITHUB URL:
+    https://github.com/Jake-77/CP386-A04.git
+-----------------------------------------------------
+
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void main(){
+    printf("Testing the Repo");
+}
