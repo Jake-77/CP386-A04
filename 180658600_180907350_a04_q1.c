6e6c57b5294e36b0abaf961153534fd05b446c0c
Jake-77 <jake.lambkin@rogers.com>
2022-03-29 21:39:09 -0400

    Update 180658600_180907350_a04_q1.c
    Struct for banker

--- 180658600_180907350_a04_q1.c
+++ 180658600_180907350_a04_q1.c
@@ -1,1 +1,18 @@
 //Github login ID's: Jake-77, maroyanp --- Github Repository URL: https://github.com/Jake-77/CP386-A04
+
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct banker //Data structures for banker to keep track of.
+{
+    int available[4];
+    int maximum[5][4];
+    int allocation[5][4];
+    int need[5][4];
+} Banker; //Created by Jake-77
+
+
+void main(){
+
+    
+}
